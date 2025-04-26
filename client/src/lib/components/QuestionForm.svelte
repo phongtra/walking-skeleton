3275b07e96a463d808df09427fb8bd368c57e981
Phong Tran <phong.tran@smartly.io>
2025-04-26 16:09:59 +0300

    add tailwind config

--- client/src/lib/components/QuestionForm.svelte
+++ client/src/lib/components/QuestionForm.svelte
@@ -3,10 +3,10 @@
     let questionState = useQuestionState();
   </script>
 
-<form onsubmit={questionState.addQestion}>
-    <label for="title">Question</label>
-    <input id="title" name="title" type="text" placeholder="Enter a new question" />
-    <label for="text">Add question text</label>
-    <textarea id="text" name="text"placeholder="Enter text of question"></textarea>
-    <input type="submit" value="Add Question" />
+<form onsubmit={questionState.addQestion} class="mx-auto w-full max-w-md space-y-4">
+    <label class="label" for="title">Question</label>
+    <input class="input" id="title" name="title" type="text" placeholder="Enter a new question" />
+    <label class="label" for="text">Add question text</label>
+    <textarea class="textarea" id="text" name="text"placeholder="Enter text of question"></textarea>
+    <input class="w-full btn preset-filled-primary-500" type="submit" value="Add Question" />
   </form>
